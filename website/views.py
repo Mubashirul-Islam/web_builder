249f43a4ee85c0d25b07ec7daa2cadb828470106
Mubashirul Islam <ishrak2nd@gmail.com>
2026-03-06 16:29:04 +0600

    class api view added

--- website/views.py
+++ website/views.py
@@ -1,4 +1,8 @@
-from rest_framework import generics, filters
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, generics, status
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
+from rest_framework.views import APIView
 from website.serializers import PageSerializer, WebsiteSerializer
 from website.models import Page, Website
 
@@ -17,16 +21,75 @@ class websiteDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Website.objects.all()
     serializer_class = WebsiteSerializer
 
-class PageList(generics.ListCreateAPIView):
+class PageList(APIView):
     '''API endpoint that allows pages to be viewed or created.'''
-    queryset = Page.objects.all()
-    serializer_class = PageSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['title']
     ordering_fields = ['created_at', 'modified_at']
     ordering = ['-modified_at']
 
-class PageDetail(generics.RetrieveUpdateDestroyAPIView):
+    def get_queryset(self):
+        return Page.objects.all()
+
+    def filter_queryset(self, queryset):
+        for backend in self.filter_backends:
+            queryset = backend().filter_queryset(self.request, queryset, self)
+        return queryset
+
+    def get_paginator(self):
+        pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
+        if not pagination_class:
+            return None
+        return pagination_class()
+
+    def get(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+        paginator = self.get_paginator()
+
+        if paginator is not None:
+            page = paginator.paginate_queryset(queryset, request, view=self)
+            if page is not None:
+                serializer = PageSerializer(page, many=True)
+                return paginator.get_paginated_response(serializer.data)
+
+        serializer = PageSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PageDetail(APIView):
     '''API endpoint that allows a single page to be viewed, updated, or deleted.'''
-    queryset = Page.objects.all()
-    serializer_class = PageSerializer
+
+    def get_object(self, pk):
+        return get_object_or_404(Page, pk=pk)
+
+    def get(self, request, pk):
+        page = self.get_object(pk)
+        serializer = PageSerializer(page)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        page = self.get_object(pk)
+        serializer = PageSerializer(page, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        page = self.get_object(pk)
+        serializer = PageSerializer(page, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        page = self.get_object(pk)
+        page.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
