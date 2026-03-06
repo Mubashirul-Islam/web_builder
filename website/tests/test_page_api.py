5f2e24a252a7735d2c38202f3a59d16c0f41cfc6
Mubashirul Islam <ishrak2nd@gmail.com>
2026-03-06 14:24:51 +0600

    tests are organized inside tests module

--- website/tests/test_page_api.py
+++ website/tests/test_page_api.py
@@ -3,77 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from website.models import Page, Website
-
-
-class WebsiteAPITests(APITestCase):
-	'''Tests for the Website API endpoints.'''
-	@classmethod
-	def setUpTestData(cls):
-		user_model = get_user_model()
-		cls.user = user_model.objects.create_user(
-			username="tester",
-			email="tester@example.com",
-			password="test-password",
-		)
-
-		cls.website_1 = Website.objects.create(
-			user=cls.user,
-			name="Alpha Site",
-			description="First",
-			url="https://alpha.example.com",
-		)
-		cls.website_2 = Website.objects.create(
-			user=cls.user,
-			name="Beta Site",
-			description="Second",
-			url="https://beta.example.com",
-		)
-
-	def test_website_list_returns_all_results(self):
-		response = self.client.get(reverse("website-list"))
-
-		self.assertEqual(response.status_code, status.HTTP_200_OK)
-		self.assertEqual(len(response.data), 2)
-
-	def test_website_list_supports_search_by_name(self):
-		response = self.client.get(reverse("website-list"), {"search": "Alpha"})
-
-		self.assertEqual(response.status_code, status.HTTP_200_OK)
-		self.assertEqual(len(response.data), 1)
-		self.assertEqual(response.data[0]["name"], "Alpha Site")
-
-	def test_website_list_honors_ordering_query_param(self):
-		response = self.client.get(reverse("website-list"), {"ordering": "created_at"})
-
-		self.assertEqual(response.status_code, status.HTTP_200_OK)
-		ordered_names = [item["name"] for item in response.data]
-		self.assertEqual(ordered_names, ["Alpha Site", "Beta Site"])
-
-	def test_website_list_supports_limit_offset_pagination(self):
-		first_page = self.client.get(
-			reverse("website-list"),
-			{"limit": 1, "offset": 0},
-		)
-
-		self.assertEqual(first_page.status_code, status.HTTP_200_OK)
-		self.assertEqual(first_page.data["count"], 2)
-		self.assertIsNone(first_page.data["previous"])
-		self.assertIn("limit=1", first_page.data["next"])
-		self.assertIn("offset=1", first_page.data["next"])
-		self.assertEqual(len(first_page.data["results"]), 1)
-		self.assertEqual(first_page.data["results"][0]["name"], "Beta Site")
-
-	def test_website_detail_returns_single_item(self):
-		response = self.client.get(reverse("website-detail", args=[self.website_1.pk]))
-
-		self.assertEqual(response.status_code, status.HTTP_200_OK)
-		self.assertEqual(response.data["id"], self.website_1.pk)
-		self.assertEqual(response.data["name"], "Alpha Site")
-
-	def test_website_detail_returns_404_for_missing_item(self):
-		response = self.client.get(reverse("website-detail", args=[99999]))
-		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+from website.models import Website, Page
 
 
 class PageAPITests(APITestCase):
